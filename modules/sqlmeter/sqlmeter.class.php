318460b46872fa755b5ee299763f8896dcbfb570
SergeJey <sergejey@gmail.com>
2021-02-01 18:08:08 +0300

    Update sqlmeter.class.php
    Missing table fix

--- modules/sqlmeter/sqlmeter.class.php
+++ modules/sqlmeter/sqlmeter.class.php
@@ -300,6 +300,16 @@ class sqlmeter extends module
  
 EOD;
         parent::dbInstall($data);
+
+        SQLExec("CREATE TABLE IF NOT EXISTS mysql.general_log(
+event_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+user_host MEDIUMTEXT NOT NULL ,
+thread_id INT( 11 ) NOT NULL ,
+server_id INT( 10 ) UNSIGNED NOT NULL ,
+command_type VARCHAR( 64 ) NOT NULL ,
+argument MEDIUMTEXT NOT NULL
+) ENGINE = CSV DEFAULT CHARSET = utf8 COMMENT = 'General log'");
+
     }
 // --------------------------------------------------------------------
 }
